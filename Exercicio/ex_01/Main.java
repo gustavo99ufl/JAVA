d37a3440c3655efe6e3a78c588f2119f77859229
Gustavo Protachevicz <gustavo_sb.blz@hotmail.com>
2025-01-17 22:11:25 -0300

    java

---
+++ Exercicio/ex_01/Main.java
@@ -0,0 +1,20 @@
+package Exercicio.ex_01;
+public class Main {
+
+    public static void main(String[] args) {
+
+        String product01 = "Computer";
+        String product02 = "Office desk";
+
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        double measure = 53.234567;
+
+        System.out.printf("Products: %n %s, which price is $ %.2f %n", product01, price1);
+        
+    }
+}
