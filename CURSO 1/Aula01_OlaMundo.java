ea1c2ecac44cb22a2b1f7b61c1ed1dd30c7ee5cb
Gustavo Protachevicz <gustavo_sb.blz@hotmail.com>
2025-01-25 09:45:58 -0300

    .

--- CURSO 1/Aula01_OlaMundo.java
+++ CURSO 1/Aula01_OlaMundo.java
@@ -1,5 +1,6 @@
 public class Aula01_OlaMundo {
     public static void main(String[] args) {
-        System.out.println("KA-ME-KA-ME-HAA")
+
+        System.out.println("KA-ME-KA-ME-HAA");
     }
 }
